45a997dfc733f558f8fb332b0354210d023f82ab
jeffkealy <jeffkealy@yahoo.com>
2023-02-22 20:36:37 -0600

    added remove list. updated js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
   var removeListLocal = JSON.parse(localStorage.getItem("removeListLocal"));
-  console.log("removeListLocal", removeListLocal);
+  // console.log("removeListLocal", removeListLocal);
 
   // var removeList = removeListLocal;
   var removeList = [];
-
+  var elements;
   // adding easier to read classes to fb's classses
+  $("<h2 class='title'>Zlist</h2>").prependTo("body");
   $(
     ".x6s0dn4.x1q0q8m5.x1qhh985.xu3j5b3.xcfux6l.x26u7qi.xm0m39n.x13fuv20.x972fbf.x9f619.x78zum5.x1q0g3np.x1iyjqo2.xs83m0k.x1qughib.xat24cr.x11i5rnm.x1mh8g0r.xdj266r.xeuugli.x18d9i69.x1sxyh0.xurb0ha.xexx8yu.x1n2onr6.x1ja2u2z.x1gg8mnh"
   ).addClass("card");
@@ -27,8 +28,15 @@ $(document).ready(function () {
   //create removed button
   $("<div class='goodbye'>Remove</div>").insertAfter(".added-by");
   // create list
-  $("<div class='remove-list'><h3>On Remove list</h3><div class='remove-list-names'></div></div>").prependTo("body");
+  $(`<div class='remove-list'>
+      <h3>On Remove list</h3>
+      <span class="copy-button">Copy</span>
+      <div class='remove-list-names'>
+      </div>
+    </div>`).prependTo("body");
   $(".name").attr("target", "_blank");
+
+  // remove button
   $(".goodbye").click(function () {
     var cardContainer = $(this).closest(".card-container");
     var goodBye = $(this);
@@ -42,24 +50,81 @@ $(document).ready(function () {
       goodBye.text("Remove");
       removeList = removeList.filter((remove) => remove.nameNoSpaces !== nameNoSpaces);
     }
-    // card not selected
+    // card not yet selected
     else {
       // console.log("added 'slected' class");
-      cardContainer.addClass("selected");
+      cardContainer.addClass(`selected ${nameNoSpaces}`);
       goodBye.text("Removed list");
       removeList.push(person);
     }
+
     // sorty a-z
     // removeList.sort((a, b) => a.nameNoSpaces.localeCompare(b.nameNoSpaces));
     $(".remove-list-names").empty();
 
     for (let i = 0; i < removeList.length; i++) {
-      $(`<div class="name-list ${removeList[i].nameNoSpaces}">${removeList[i].name}</div>`).appendTo(".remove-list-names");
+      $(`
+      <div class="name-list-entry ${removeList[i].nameNoSpaces}">
+        <span class="x-off">X</span>
+        <span class="name-list ${removeList[i].nameNoSpaces}">
+        ${removeList[i].name} 
+        </span>
+      <div>  
+        `).appendTo(".remove-list-names");
     }
+
+    // x-off button
+    $(".name-list-entry").click(function () {
+      let classes = $(this).attr("class");
+      let split = classes.split(" ");
+      let nameNoSpaces = split[1];
+      $(`.card-container.${nameNoSpaces} .goodbye`).text("Remove");
+      $(`.card-container.${nameNoSpaces}`).removeClass("selected");
+      // console.log("NameListremoveList before", removeList);
+      removeList = removeList.filter((remove) => remove.nameNoSpaces !== nameNoSpaces);
+
+      $(this).remove();
+      elements = $(".name-list"); //generate list of names on remove list for copy button
+      console.log("removeList", removeList);
+      // console.log("NameListremoveList", removeList);
+    });
+
+    elements = $(".name-list"); //generate list of names on remove list for copy button
     console.log("removeList", removeList);
-    localStorage.setItem("removeListLocal", JSON.stringify(removeList));
+    // localStorage.setItem("removeListLocal", JSON.stringify(removeList));
 
     // console.log("localstorage", JSON.parse(localStorage.getItem("removeListLocal")));
     // $(this).css();
   });
+
+  // copy button
+  $(".copy-button").click(function () {
+    if (elements) {
+      let copiedText = "";
+      elements.each(function () {
+        // copiedText += $(this).text() + "\n";
+        copiedText += $(this).text();
+      });
+      console.log("copiedText", copiedText);
+      // Create a temporary input element to copy the text to the clipboard
+      const tempInput = $("<textarea>");
+      tempInput.val(copiedText);
+      $("body").append(tempInput);
+      // Select the contents of the temporary input element
+      tempInput.select();
+      // Copy the text to the clipboard
+      document.execCommand("copy");
+      // Remove the temporary input element from the DOM
+      tempInput.remove();
+      $(".copy-button").text("Copied");
+      setTimeout(function () {
+        $(".copy-button").text("Copy");
+      }, 800);
+    } else {
+      console.log("no text");
+    }
+  });
 });
+// function removeFromRemoveList() {
+//   console.log("removeFromRemoveList");
+// }
